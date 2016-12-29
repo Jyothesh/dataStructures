e907a6b83dc31c09e368fc53ed3cde825804831a
Jyothesh Kongara <jyothesh@bitbucket.org>
2016-12-29 19:08:46 +0530

    Added node class and added printList().

--- linkedlist/src/main/java/com/jyothesh/LinkedList.java
+++ linkedlist/src/main/java/com/jyothesh/LinkedList.java
@@ -2,9 +2,28 @@ package com.jyothesh;
 
 public class LinkedList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Node head;
+	
+	public void printList() {
+		Node nextNode = head;
+		
+		while(nextNode != null){
+			System.out.print(nextNode.data + " ");
+			nextNode = nextNode.next;
+		}
 	}
-
+	
+	public static void main(String args[]) {
+		LinkedList list = new LinkedList();
+		
+		list.head = new Node(1);
+		Node second = new Node(2);
+		Node three = new Node(3);
+		
+		list.head.next = second;
+		second.next = three;
+		
+		list.printList();
+	}
+	
 }
